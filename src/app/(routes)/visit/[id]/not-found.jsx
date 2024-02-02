30f373a679222f73aba8ae7184ee21e36965add7
Glade3796 <151785784+Glade3796@users.noreply.github.com>
2024-02-02 21:29:06 +0000

    changed user not found font color

--- src/app/(routes)/visit/[id]/not-found.jsx
+++ src/app/(routes)/visit/[id]/not-found.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found</h2>
-      <p>User not found</p>
-      <Link href="/">Return to the homepage</Link>
+      <h2 className="text-white">Not Found</h2>
+      <p className="text-white">User not found</p>
+      <Link href="/" className="text-white">
+        Return to the homepage
+      </Link>
     </div>
   );
 }
